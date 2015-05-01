c258717fbc72c589a518443ab5c3f7cd98f95cc5
Clint Chilcott <clintchil@gmail.com>
2015-05-01 16:50:36 -0700

    Initial Commit

---
+++ ShowerThoughts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShowerThoughts
+//
+//  Created by Clint Chilcott on 5/1/15.
+//  Copyright (c) 2015 Clint Chilcott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
