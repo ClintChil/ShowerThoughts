3802408911c2dc7e180d44f24f8619d53475884b
Clint Chilcott <clintchil@gmail.com>
2015-05-14 23:19:24 -0700

    adding buttons

---
+++ ShowerThoughts WatchKit Extension/MainRowType.h
@@ -0,0 +1,17 @@
+//
+//  MainRowType.h
+//  ShowerThoughts
+//
+//  Created by Clint Chilcott on 5/14/15.
+//  Copyright (c) 2015 Clint Chilcott. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <WatchKit/WatchKit.h>
+
+@interface MainRowType : NSObject
+
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *postBodyLabel;
+@property (weak, nonatomic) IBOutlet WKInterfaceDate *postDate;
+
+@end
---
+++ ShowerThoughts WatchKit Extension/MainRowType.m
@@ -0,0 +1,13 @@
+//
+//  MainRowType.m
+//  ShowerThoughts
+//
+//  Created by Clint Chilcott on 5/14/15.
+//  Copyright (c) 2015 Clint Chilcott. All rights reserved.
+//
+
+#import "MainRowType.h"
+
+@implementation MainRowType
+
+@end
