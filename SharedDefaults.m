48a406888b80ffa6a030f868c6c499c4a9952b01
Rich Fellure <rfellure24@gmail.com>
2015-06-13 16:41:14 -0700

    Saving the user info in user defaults.

---
+++ SharedDefaults.m
@@ -0,0 +1,13 @@
+//
+//  SharedDefaults.m
+//  ShowerThoughts
+//
+//  Created by Rich Fellure on 6/13/15.
+//  Copyright © 2015 Clint Chilcott. All rights reserved.
+//
+
+#import "SharedDefaults.h"
+
+@implementation SharedDefaults
+
+@end
