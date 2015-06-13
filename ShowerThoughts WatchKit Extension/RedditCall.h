fdc5e44654f0a1b4d71bb3d55040a4f77c49264b
Rich Fellure <rfellure24@gmail.com>
2015-06-13 12:22:52 -0700

    Working to make stuff work.

---
+++ ShowerThoughts WatchKit Extension/RedditCall.h
@@ -0,0 +1,13 @@
+//
+//  RedditCall.h
+//  ShowerThoughts
+//
+//  Created by Rich Fellure on 6/13/15.
+//  Copyright (c) 2015 Clint Chilcott. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RedditCall : NSObject
+
+@end
---
+++ ShowerThoughts WatchKit Extension/RedditCall.m
@@ -0,0 +1,13 @@
+//
+//  RedditCall.m
+//  ShowerThoughts
+//
+//  Created by Rich Fellure on 6/13/15.
+//  Copyright (c) 2015 Clint Chilcott. All rights reserved.
+//
+
+#import "RedditCall.h"
+
+@implementation RedditCall
+
+@end
