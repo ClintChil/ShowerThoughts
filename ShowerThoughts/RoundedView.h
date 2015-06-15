6d3b5c0db9ed4fdd3a565811592ffa2fb4664847
Rich Fellure <rfellure24@gmail.com>
2015-06-15 01:05:13 -0700

    Feed looks better. Getting close now.

---
+++ ShowerThoughts/RoundedView.h
@@ -0,0 +1,13 @@
+//
+//  RoundedView.h
+//  ShowerThoughts
+//
+//  Created by Rich Fellure on 6/15/15.
+//  Copyright (c) 2015 Clint Chilcott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+IB_DESIGNABLE
+@interface RoundedView : UIButton
+
+@end
